bf4111261a0632d26aa6ac967c9307ed4ea1d8e8
Leandro Dotta <leandro@leandro-note-ubuntu>
2020-02-01 11:25:40 -0300

    Update CharacterScene.unity, PlayerController.cs, and Assembly-CSharp.csprojAssemblyReference.cache

--- GlobalGameJam2020/Assets/Scripts/PlayerController.cs
+++ GlobalGameJam2020/Assets/Scripts/PlayerController.cs
@@ -4,11 +4,59 @@ using UnityEngine;
 
 public class PlayerController : MonoBehaviour
 {
-    private void Start() {
-        
+    [SerializeField] private float speed;
+    [SerializeField] private float jumpForce;
+
+    [SerializeField] Collider2D groundCheck;
+
+    private float axisHorizontal;
+
+    private bool inputJump;
+    private bool grounded = true;
+
+    private Rigidbody2D rb2d;
+
+    private void Start()
+    {
+        rb2d = GetComponent<Rigidbody2D>();
     }
+
+    private void FixedUpdate()
+    {
+        Jump();
+    }
+
+    private void Update() 
+    {
+        axisHorizontal = Input.GetAxisRaw("Horizontal");
+        inputJump = Input.GetButtonDown("Jump");
+
+        Move();
+        Flip();
+    }
+    
     private void Move()
     {
+        Vector2 movement = new Vector2(axisHorizontal, 0) * speed * Time.deltaTime;
+        transform.Translate(movement);
+    }
 
+    private void Flip()
+    {
+        if (axisHorizontal != 0)
+        {
+            Vector3 scale = transform.localScale;
+            scale.x = axisHorizontal;
+            transform.localScale = scale;
+        }
+    }
+
+    private void Jump()
+    {
+        if (inputJump && grounded)
+        {
+            inputJump = false;
+            rb2d.AddForce(Vector2.up * jumpForce, ForceMode2D.Impulse);
+        }
     }
 }
