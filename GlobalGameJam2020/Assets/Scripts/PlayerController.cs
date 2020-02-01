23522a5efbfeb333e1e981db3356a2022ab0e316
Leandro Dotta <leandro@leandro-note-ubuntu>
2020-02-01 19:28:20 -0300

    Created the Stair prefab

--- GlobalGameJam2020/Assets/Scripts/PlayerController.cs
+++ GlobalGameJam2020/Assets/Scripts/PlayerController.cs
@@ -5,16 +5,22 @@ using UnityEngine;
 public class PlayerController : MonoBehaviour
 {
     [SerializeField] private float speed;
+    [SerializeField] private float climbSpeed;
     [SerializeField] private float jumpForce;
+
 
     [SerializeField] Collider2D groundCheck;
 
     private float axisHorizontal;
-
+    private float axisVertical;
     private bool inputJump;
+
     private bool grounded = true;
+    private bool climbing;
+    private bool canClimb;
 
     private Rigidbody2D rb2d;
+    private Collider2D stair;
 
     private void Start()
     {
@@ -26,21 +32,61 @@ public class PlayerController : MonoBehaviour
         Jump();
     }
 
-    private void Update() 
+    private void Update()
     {
         axisHorizontal = Input.GetAxisRaw("Horizontal");
+        axisVertical = Input.GetAxisRaw("Vertical");
 
-        if (!inputJump && grounded)
+        if (!inputJump && (grounded || climbing))
             inputJump = Input.GetButtonDown("Jump");
+
+        if (canClimb)
+        {
+            if (!climbing && axisVertical > 0)
+            {
+                StartClimb();
+            }
+        }
 
         Move();
         Flip();
     }
-    
+
+    private void StartClimb()
+    {
+        rb2d.isKinematic = true;
+        rb2d.velocity = Vector2.zero;
+        Vector3 position = transform.position;
+        position.x = stair.transform.position.x;
+        transform.position = position;
+
+        climbing = true;
+    }
+
+    private void StopClimb()
+    {
+        rb2d.isKinematic = false;
+        climbing = false;
+    }
+
     private void Move()
     {
-        Vector2 movement = new Vector2(axisHorizontal, 0) * speed * Time.deltaTime;
-        transform.Translate(movement);
+        if (climbing)
+        {
+            Vector2 movement = new Vector2(0, axisVertical) * climbSpeed * Time.deltaTime;
+            transform.Translate(movement);
+
+            if (grounded || !canClimb)
+            {
+                StopClimb();
+            }
+        }
+        else
+        {
+            Vector2 movement = new Vector2(axisHorizontal, 0) * speed * Time.deltaTime;
+            transform.Translate(movement);
+        }
+
     }
 
     private void Flip()
@@ -53,26 +99,49 @@ public class PlayerController : MonoBehaviour
         }
     }
 
+
+
     private void Jump()
     {
         if (inputJump)
         {
             inputJump = false;
+            StopClimb();
             rb2d.AddForce(Vector2.up * jumpForce, ForceMode2D.Impulse);
         }
     }
 
     private void OnTriggerEnter2D(Collider2D other)
     {
-        Debug.Log("Grounded!");
-        //grounded = other.CompareTag("Ground");
-        grounded = true;
+        if (other.CompareTag("Ground"))
+        {
+            grounded = true;
+        }
+
+        if (other.CompareTag("Stair"))
+        {
+            canClimb = true;
+            stair = other;
+        }
+
     }
 
     private void OnTriggerExit2D(Collider2D other)
     {
-        Debug.Log("Grounded!");
-        //grounded = other.CompareTag("Ground");
-        grounded = false;
+        if (other.CompareTag("Ground"))
+        {
+            grounded = false;
+        }
+
+        if (other.CompareTag("Stair"))
+        {
+            canClimb = false;
+        }
+    }
+
+    private void OnTriggerStay2D(Collider2D other)
+    {
+        grounded = other.CompareTag("Ground");
+        canClimb = other.CompareTag("Stair");
     }
 }
---
+++ GlobalGameJam2020/Assets/Scripts/Stair.cs
@@ -0,0 +1,60 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.Events;
+
+
+
+public class Stair : MonoBehaviour
+{
+    [SerializeField] Collider2D platformCollider;
+
+    private float horizontalRotation = 0;
+    private float verticalRotation = 90;
+
+    private Orientation orientation;
+
+    private int layerDefault;
+    private int layerIgnorePlayer;
+    
+    private void Start()
+    {
+        layerDefault = LayerMask.NameToLayer("Default");
+        layerIgnorePlayer = LayerMask.NameToLayer("IgnorePlayer");
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if (Input.GetKeyDown(KeyCode.R))
+        {
+            if (orientation == Orientation.Horizontal)
+                SetOrientation(Orientation.Vertical);
+            else
+                SetOrientation(Orientation.Horizontal);
+        }
+    }
+
+    public void SetOrientation(Orientation orientation)
+    {
+        this.orientation = orientation;
+
+        if (orientation == Orientation.Horizontal)
+        {
+            transform.localRotation = Quaternion.Euler(0, 0, horizontalRotation);
+            platformCollider.gameObject.layer = layerDefault;
+        }
+        else
+        {
+            transform.localRotation = Quaternion.Euler(0, 0, verticalRotation);
+            platformCollider.gameObject.layer = layerIgnorePlayer;
+        }
+
+    }
+
+    public enum Orientation
+    {
+        Vertical,
+        Horizontal
+    }
+}
