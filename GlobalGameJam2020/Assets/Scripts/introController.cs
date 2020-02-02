6d5afe7b58f9dbbf3add31c11da72082a4b2b4ca
curicom <curicom.games@gmail.com>
2020-02-02 13:23:35 -0300

    som power up

--- GlobalGameJam2020/Assets/Scripts/introController.cs
+++ GlobalGameJam2020/Assets/Scripts/introController.cs
@@ -5,21 +5,28 @@ using UnityEngine.SceneManagement;
 
 public class introController : MonoBehaviour
 {
-    // Start is called before the first frame update
-    void Start()
-    {
-        
-    }
 
-    // Update is called once per frame
-    void Update()
-    {
-        
-    }
+    public AudioSource sfxButtonAS;
+
+    public AudioClip sfxButtonClickAC;
+    public AudioClip sfxButtonHoverAC;
+
 
     public void carregaCena(string cena)
     {
         SceneManager.LoadScene(cena);
     }
+
+
+    public void PlaySfxButtonClick()
+    {
+        sfxButtonAS.PlayOneShot(sfxButtonClickAC);
+    }
+
+    public void PlaySfxButtonHover()
+    {
+        sfxButtonAS.PlayOneShot(sfxButtonHoverAC);
+    }
+
 
 }
