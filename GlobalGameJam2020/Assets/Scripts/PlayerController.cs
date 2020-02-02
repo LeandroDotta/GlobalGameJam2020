5d75644a02e66c98663ae645ff06fc5e4e1d792d
curicom <curicom.games@gmail.com>
2020-02-02 13:34:23 -0300

    arrumado som do power up

--- GlobalGameJam2020/Assets/Scripts/PlayerController.cs
+++ GlobalGameJam2020/Assets/Scripts/PlayerController.cs
@@ -266,7 +266,7 @@ public class PlayerController : MonoBehaviour
             if (pickup.restore)
                 health.Restore(pickup.health);
 
-            SoundController.Instance.PlaySFX(11);
+            SoundController.Instance.PlaySFX(10);
 
             Destroy(pickup.gameObject);
         }
