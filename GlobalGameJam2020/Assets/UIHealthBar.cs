99874d9e787ce67f26559751160f5d224152adc8
Leandro Dotta <leandro@leandro-note-ubuntu>
2020-02-02 14:34:36 -0300

    HUD and interface

---
+++ GlobalGameJam2020/Assets/UIHealthBar.cs
@@ -0,0 +1,9 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class UIHealthBar : MonoBehaviour
+{
+    [Range(0, 1)]
+    private float emptyTransparancy = 0.3f;
+}
