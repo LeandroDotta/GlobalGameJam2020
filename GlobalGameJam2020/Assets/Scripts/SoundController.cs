8502f675afe6688b60e220b2abc40b2a6a86095f
curicom <curicom.games@gmail.com>
2020-02-02 07:29:29 -0300

    soundcontroller script

--- GlobalGameJam2020/Assets/Scripts/SoundController.cs
+++ GlobalGameJam2020/Assets/Scripts/SoundController.cs
@@ -3,7 +3,20 @@ using System.Collections.Generic;
 using UnityEngine;
 
 public class SoundController : MonoBehaviour
+
 {
+    //AudioSource Sources
+    public AudioSource sfxAS;
+    //sfxAS[0]:pulo1; sfxAS[1]:pulo2; sfxAS[2]:pulo3; sfxAS[3]:tiro1; sfxAS[4]:tiro2; sfxAS[5]:tiro3; 
+    public AudioSource musicAS;
+    //mucicAS[0]: Musica Tema (Intro); mucicAS[1]: Musica Stage (Fase); mucicAS[2]: Fanfarra Vitória; mucicAS[3]: Fanfarra Game Over
+
+    //AudioClips
+    public AudioClip [] sfxAC;
+    public AudioClip[] musicAC;
+
+
+
     // Start is called before the first frame update
     void Start()
     {
