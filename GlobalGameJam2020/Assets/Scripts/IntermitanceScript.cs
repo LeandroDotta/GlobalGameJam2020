9cf1dfde6a18d532768acaef9ef174b7860e8486
yotagah <jhon.hnng@gmail.com>
2020-02-07 14:43:21 -0300

    Short Circuit obstacle implementation. Changes in Hazard and Player scripts were needed!

---
+++ GlobalGameJam2020/Assets/Scripts/IntermitanceScript.cs
@@ -0,0 +1,40 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class IntermitanceScript : MonoBehaviour
+{
+	public int beginTime = 0;
+	public int onTime = 10;
+	public int offTime = 0;
+
+	private Hazard hazard;
+	private float timer;
+	// Start is called before the first frame update
+	void Start()
+	{
+		hazard = GetComponent<Hazard>();
+		hazard.on = false;
+		timer = beginTime;
+	}
+
+	// Update is called once per frame
+	void Update()
+	{
+		timer -= Time.deltaTime;
+		if (timer <= 0.0f)
+		{
+			Switch();
+		}
+	}
+
+	void Switch() {
+		if(hazard.on) {
+			timer = offTime;
+		} else {
+			timer = onTime;
+		}
+
+		hazard.on = !hazard.on;
+	}
+}
---
+++ GlobalGameJam2020/Assets/Scripts/ShortCircuitScript.cs
@@ -0,0 +1,50 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class ShortCircuitScript : MonoBehaviour
+{
+    private Hazard hazard;
+    public ParticleSystem particle;
+    // Start is called before the first frame update
+    void Start()
+    {
+        hazard = transform.parent.GetComponent<Hazard>();
+        particle.Stop();
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if(hazard.on) {
+            transform.localScale = new Vector3(Random.Range(0.2f,0.7f), 1.0f, 1.0f);
+            ShowLightning(Random.Range(0,4));
+            if(!particle.isPlaying) {
+                particle.Play();
+            }
+        } else {
+            TurnOffLightning();
+            particle.Stop();
+        }
+    }
+
+    void ShowLightning(int number)
+    {
+        int x;
+
+        for(x = 0; x < 4; x++) {
+            if(x == number)
+                transform.GetChild(x).GetComponent<SpriteRenderer>().enabled = true;
+            else
+                transform.GetChild(x).GetComponent<SpriteRenderer>().enabled = false;
+        }
+    }
+
+    void TurnOffLightning() {
+        int x;
+
+        for(x = 0; x < 4; x++) {
+            transform.GetChild(x).GetComponent<SpriteRenderer>().enabled = false;
+        }
+    }
+}
